bcbf450f4d4064dba35d90f95ba073e8e2d42ef0
eya abbes <eya.abbes@insat.ucar.tn>
2026-03-27 14:38:25 +0100

    petite modif

--- php/demande.php
+++ php/demande.php
@@ -1,4 +1,6 @@
 <?php 
+session_start();
+
 $nom_produit=$_POST["nom_produit"]?? '';
 $prix=$_POST["prix"]?? '';
 $categorie=$_POST["categorie"]?? '';
@@ -9,9 +11,12 @@ $newFilePath=null;
         $newFilePath = "../files_demande/" . uniqid() . "_" . basename($_FILES['image']['name']);
         move_uploaded_file($_FILES['image']['tmp_name'], $newFilePath);
     }
+
 require_once("connexionBD.php");
 $bdd=ConnexionBD::getInstance();
-$red=$bdd->prepare("insert into demande(nom_produit,prix,lien_produit,description,categorie,id_photo) values (:nom_produit,:prix,:lien_produit,:description,:categorie,:id_photo)");
-$red->execute(array("nom_produit"=>$nom_produit,"prix"=>$prix,"lien_produit"=>$lien_produit,"description"=>$description,"categorie"=>$categorie,"id_photo"=>$newFilePath));
+$etat="en attente";
+$username=$_SESSION['user']["username"];
+$red=$bdd->prepare("insert into demande(nom_produit,prix,lien_produit,description,categorie,id_photo,username,etat) values (:nom_produit,:prix,:lien_produit,:description,:categorie,:id_photo,:username,:etat)");
+$red->execute(array("nom_produit"=>$nom_produit,"prix"=>$prix,"lien_produit"=>$lien_produit,"description"=>$description,"categorie"=>$categorie,"id_photo"=>$newFilePath,"username"=>$username,"etat"=>$etat));
 header("location:page_client.php");
 ?>
